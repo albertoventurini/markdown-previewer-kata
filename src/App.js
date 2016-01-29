74a367df6d4d39b01e67c6df44ad6143f33ec2e1
Alberto Venturini <alberto.venturini@codiceplastico.com>
2016-01-29 16:56:42 +0100

    Main layout and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Editor from './Editor';
+import Previewer from './Previewer';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div style={{display: 'table', width: '100%'}}>
+        <div style={{display: 'table-row'}}>
+          <div style={{display: 'table-cell', width: '50%'}}>
+            <Editor />
+          </div>
+          <div style={{display: 'table-cell', width: '50%'}}>
+            <Previewer />
+          </div>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/Previewer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Previewer extends Component {
+  render() {
+    return (
+      <h2>Previewer here!</h2>
+    );
+  }
+}
